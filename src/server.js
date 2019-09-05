c569f36309910e6112c9baacd4d185fb11279353
Timo Pitkänen <timo.pitkanen@iki.fi>
2019-09-05 13:53:02 +0300

    Score max

--- src/server.js
+++ src/server.js
@@ -89,6 +89,10 @@ function sendScore(){
   for(let i=0; i<maxclients; i++) {
     if(clients[i]) clients[i].connection.sendUTF(data);
   }
+  if(scoreRed>=9 || scoreBlue>=9){
+    scoreRed = 0;
+    scoreBlue = 0;
+  }
 }
 
 function goal() {
